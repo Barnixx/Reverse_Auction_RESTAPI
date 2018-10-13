630d3df8329aac3a19ba50818c9d5be662c2cce6
Barnixx <rb.kuszaj@gmail.com>
2018-10-13 21:26:17 +0200

    Created /api/users/ get mapping action and implemented Unit tests.

--- src/main/java/pl/barnixx/reverse_auction/api/controllers/UserController.java
+++ src/main/java/pl/barnixx/reverse_auction/api/controllers/UserController.java
@@ -4,12 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import pl.barnixx.reverse_auction.core.domain.User;
 import pl.barnixx.reverse_auction.infrastructure.DTO.UserDTO;
 import pl.barnixx.reverse_auction.infrastructure.commands.ICommandDispatcher;
 import pl.barnixx.reverse_auction.infrastructure.commands.users.CreateUser;
 import pl.barnixx.reverse_auction.infrastructure.services.IUserService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -26,12 +28,17 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> get(@PathVariable long id) {
         UserDTO user = userService.findById(id);
-
         if (user == null) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
 
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> get() {
+        List<User> users = userService.getAll();
+        return new ResponseEntity<>(users, HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping
